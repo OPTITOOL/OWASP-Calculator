070e787ba77c3d63d4cbce7bb7bf2eeeabeb890c
Sven Jäger <sven.jaeger@optitool.de>
2025-04-25 16:38:02 +0200

    Refactor code formatting for consistency and readability in script.js

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@
 var riskChart = document.getElementById('riskChart').getContext('2d');
 
 const colors = [
-'rgba(255, 102, 255)',
-'rgba(255, 0, 0)',
-'rgba(255, 169, 0)',
-'rgba(255, 255, 0)',
-'rgba(144, 238, 144)'
+  'rgba(255, 102, 255)',
+  'rgba(255, 0, 0)',
+  'rgba(255, 169, 0)',
+  'rgba(255, 255, 0)',
+  'rgba(144, 238, 144)'
 ];
 
 const backgrounds = [
@@ -20,9 +20,9 @@ const backgrounds = [
 ];
 
 const threats = ["Skills required", "Motive", "Opportunity", "Population Size",
-"Easy of Discovery", "Ease of Exploit", "Awareness", "Intrusion Detection",
-"Loss of confidentiality", "Loss of Integrity", "Loss of Availability", "Loss of Accountability",
-"Financial damage", "Reputation damage", "Non-Compliance", "Privacy violation"
+  "Easy of Discovery", "Ease of Exploit", "Awareness", "Intrusion Detection",
+  "Loss of confidentiality", "Loss of Integrity", "Loss of Availability", "Loss of Accountability",
+  "Financial damage", "Reputation damage", "Non-Compliance", "Privacy violation"
 ];
 
 const partials = ["sl", "m", "o", "s", "ed", "ee", "a", "id", "lc", "li", "lav", "lac", "fd", "rd", "nc", "pv"];
@@ -64,7 +64,7 @@ riskChart = new Chart(riskChart, {
 
 updateRiskChart()
 
-if(getUrlParameter('vector')){
+if (getUrlParameter('vector')) {
   loadVectors(getUrlParameter('vector'))
 }
 
@@ -75,11 +75,11 @@ function loadVectors(vector) {
   var values = vector.split('/');
 
   if (values.length == 16) {
-    for (let i=0; i<values.length; i++) {
-        let aux = values[i].split(':');
-        let vector = aux[1];
-        console.log(vector)
-        $("#" + partials[i].toLowerCase()).val(vector);
+    for (let i = 0; i < values.length; i++) {
+      let aux = values[i].split(':');
+      let vector = aux[1];
+      console.log(vector)
+      $("#" + partials[i].toLowerCase()).val(vector);
     }
   } else {
     swal("Hey!!", "The vector is not correct, make sure you have copied correctly", "error");
@@ -88,7 +88,7 @@ function loadVectors(vector) {
   calculate()
 }
 
-function calculate(){
+function calculate() {
   var LS = 0;
   var IS = 0;
   var dataset = [];
@@ -97,13 +97,13 @@ function calculate(){
 
   // Get values THREAT AGENT FACTORS and VULNERABILITY FACTORS
   LS = + $("#sl").val() +
-  + $("#m").val() +
-  + $("#o").val() +
-  + $("#s").val() +
-  + $("#ed").val() +
-  + $("#ee").val() +
-  + $("#a").val() +
-  + $("#id").val() + 0;
+    + $("#m").val() +
+    + $("#o").val() +
+    + $("#s").val() +
+    + $("#ed").val() +
+    + $("#ee").val() +
+    + $("#a").val() +
+    + $("#id").val() + 0;
   dataset.push($("#sl").val());
   dataset.push($("#m").val());
   dataset.push($("#o").val());
@@ -115,13 +115,13 @@ function calculate(){
 
   // Get values TECHNICAL IMPACT FACTORS and BUSINESS IMPACT FACTORS
   IS = + $("#lc").val() +
-  + $("#li").val() +
-  + $("#lav").val() +
-  + $("#lac").val() +
-  + $("#fd").val() +
-  + $("#rd").val() +
-  + $("#nc").val() +
-  + $("#pv").val() + 0;
+    + $("#li").val() +
+    + $("#lav").val() +
+    + $("#lac").val() +
+    + $("#fd").val() +
+    + $("#rd").val() +
+    + $("#nc").val() +
+    + $("#pv").val() + 0;
   dataset.push($("#lc").val());
   dataset.push($("#li").val());
   dataset.push($("#lav").val());
@@ -130,9 +130,9 @@ function calculate(){
   dataset.push($("#rd").val());
   dataset.push($("#nc").val());
   dataset.push($("#pv").val());
-  
-  var LS = (LS/8).toFixed(3);
-  var IS = (IS/8).toFixed(3);
+
+  var LS = (LS / 8).toFixed(3);
+  var IS = (IS / 8).toFixed(3);
 
   var FLS = getRisk(LS);
   var FIS = getRisk(IS);
@@ -161,78 +161,78 @@ function calculate(){
   $('#score').text(score);
   $("#score").attr("href", "https://optitool.github.io/OWASP-Calculator/?vector=" + score);
 
-  if(getRisk(LS) == "LOW"){
-      $(".LS").addClass("classNote");
-  } else if (getRisk(LS) == "MEDIUM"){
-      $(".LS").addClass("classMedium");
+  if (getRisk(LS) == "LOW") {
+    $(".LS").addClass("classNote");
+  } else if (getRisk(LS) == "MEDIUM") {
+    $(".LS").addClass("classMedium");
   } else {
-      $(".LS").addClass("classHigh");
+    $(".LS").addClass("classHigh");
   }
 
-  if(getRisk(IS) == "LOW"){
-      $(".IS").addClass("classNote");
-  } else if (getRisk(IS) == "MEDIUM"){
-      $(".IS").addClass("classMedium");
+  if (getRisk(IS) == "LOW") {
+    $(".IS").addClass("classNote");
+  } else if (getRisk(IS) == "MEDIUM") {
+    $(".IS").addClass("classMedium");
   } else {
-      $(".IS").addClass("classHigh");
+    $(".IS").addClass("classHigh");
   }
 
   //FINAL
   var RS = getCriticaly(FLS, FIS);
-  if(RS == "NOTE"){
-      $(".RS").text(RS);
-      $(".RS").addClass("classNote");
-  } else if (RS == "LOW"){
-      $(".RS").text(RS);
-      $(".RS").addClass("classLow");
-  } else if(RS == "MEDIUM"){
-      $(".RS").text(RS);
-      $(".RS").addClass("classMedium");
-  } else if(RS == "HIGH"){
-      $(".RS").text(RS);
-      $(".RS").addClass("classHigh");
-  } else if(RS == "CRITICAL"){
-      $(".RS").text(RS);
-      $(".RS").addClass("classCritical");
+  if (RS == "NOTE") {
+    $(".RS").text(RS);
+    $(".RS").addClass("classNote");
+  } else if (RS == "LOW") {
+    $(".RS").text(RS);
+    $(".RS").addClass("classLow");
+  } else if (RS == "MEDIUM") {
+    $(".RS").text(RS);
+    $(".RS").addClass("classMedium");
+  } else if (RS == "HIGH") {
+    $(".RS").text(RS);
+    $(".RS").addClass("classHigh");
+  } else if (RS == "CRITICAL") {
+    $(".RS").text(RS);
+    $(".RS").addClass("classCritical");
   } else {
-      $(".RS").text(RS);
-      $(".RS").addClass("classNote");
+    $(".RS").text(RS);
+    $(".RS").addClass("classNote");
   }
 
   updateRiskChart(dataset, RS)
 }
 
-function getRisk(score){
-  if(score == 0) return 'NOTE';
-  if(score < 3) return 'LOW';
-  if(score < 6) return 'MEDIUM';
-  if(score <= 9) return 'HIGH';
+function getRisk(score) {
+  if (score == 0) return 'NOTE';
+  if (score < 3) return 'LOW';
+  if (score < 6) return 'MEDIUM';
+  if (score <= 9) return 'HIGH';
 }
 
 // Calculate final Risk Serverity
-function getCriticaly(L, I){
+function getCriticaly(L, I) {
   //NOTE
-  if(L == "LOW" && I == "LOW") return 'NOTE';
+  if (L == "LOW" && I == "LOW") return 'NOTE';
 
   //LOW
-  if(L == "LOW" && I == "MEDIUM") return 'LOW';
-  if(L == "MEDIUM" && I == "LOW") return 'LOW';
-  
+  if (L == "LOW" && I == "MEDIUM") return 'LOW';
+  if (L == "MEDIUM" && I == "LOW") return 'LOW';
+
   //MEDIUM
-  if(L == "LOW" && I == "HIGH") return 'MEDIUM';
-  if(L == "MEDIUM" && I == "MEDIUM") return 'MEDIUM';
-  if(L == "HIGH" && I == "LOW") return 'MEDIUM';
+  if (L == "LOW" && I == "HIGH") return 'MEDIUM';
+  if (L == "MEDIUM" && I == "MEDIUM") return 'MEDIUM';
+  if (L == "HIGH" && I == "LOW") return 'MEDIUM';
 
   //HIGH
-  if(L == "HIGH" && I == "MEDIUM") return 'HIGH';
-  if(L == "MEDIUM" && I == "HIGH") return 'HIGH';
+  if (L == "HIGH" && I == "MEDIUM") return 'HIGH';
+  if (L == "MEDIUM" && I == "HIGH") return 'HIGH';
 
   //CRITICAL
-  if(L == "HIGH" && I == "HIGH") return 'CRITICAL';
+  if (L == "HIGH" && I == "HIGH") return 'CRITICAL';
 }
 
 // Delete class before of calculate
-function deleteClass(){
+function deleteClass() {
   // Delete Class Likelihood Score
   $(".LS").removeClass("classNote");
   $(".LS").removeClass("classMedium");
@@ -258,7 +258,7 @@ function getUrlParameter(name) {
   return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
 
-function updateRiskChart(dataset, RS){
+function updateRiskChart(dataset, RS) {
   var c = 0;
   var dataset = dataset;
 
